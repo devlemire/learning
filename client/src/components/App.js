6d5c1317f96e7344d55c71f6a6430b113c71670a
James Lemire <webdev.jameslemire@gmail.com>
2021-03-16 11:55:12 -0400

    Allow cors origin requests so React client can call spring endpoint

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+
 function App() {
+    useEffect(() => {
+        const asyncFetchStudents = async () => {
+            const resp = await fetch('http://localhost:8080/api/v1/student')
+            console.log('resp', resp)
+
+            const result = await resp.json()
+
+            console.log('result', result)
+        }
+
+        asyncFetchStudents();
+    }, [])
+
   return (
     <div className="App">
-      <p>Hello World</p>
+      <p>Hello Worlddddddd</p>
     </div>
   );
 }
